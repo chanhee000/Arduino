686ed9c46cedaa8fef6963e7475ea6013ada0ab9
chanhee000 <125625470+chanhee000@users.noreply.github.com>
2023-03-14 15:37:13 +0900

    Add files via upload

---
+++ 20230314/Sonar1/Sonar1.ino
@@ -0,0 +1,27 @@
+#define TRIG1 3  // 초음파 센서 1번 Trig 핀 번호
+#define ECHO1 4  // 초음파 센서 1번 Echo 핀 번호
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(TRIG1, OUTPUT);
+  pinMode(ECHO1, INPUT);
+
+  Serial.begin(115200); // 통신 속도를 115200으로 정의함
+}
+long sonar1(void)
+{
+  long duration, distance;
+  digitalWrite(TRIG1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG1,LOW);
+  duration = pulseIn(ECHO1, HIGH);
+  distance = ( (float) (340*duration)/1000)/2;
+  return distance;
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Serial.print("Duration: ");
+  //Serial.println(duration);
+  Serial.print("Distance: ");
+  Serial.println(sonar1()  );
+  delay(500);
+}
